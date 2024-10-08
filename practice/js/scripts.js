aa9075cb5d0725c624cc4a4e33ef5a11e905d53f
sil <narradoresdigitales@gmail.com>
2024-10-08 10:28:11 -0500

    working through practice videos, modals

--- practice/js/scripts.js
+++ practice/js/scripts.js
@@ -1,12 +1,26 @@
 const openButton = document.querySelector("#openButton");
 const dialogBox = document.querySelector("#dialogBox");
 const closeButton = document.querySelector("#closeButton");
+const dialogBoxText = document.querySelector("#dialogBox div");
 
 // open modal
-openButton.addEventListener("click", () => {
+openButton1.addEventListener("click", () => {
     dialogBox.showModal();
-
+    dialogBoxText.innerHTML = `One Apple contains 95 calories`
 });
+
+openButton2.addEventListener("click", () => {
+    dialogBox.showModal();
+    dialogBoxText.innerHTML = `One Orange contains 45 calories`
+});
+
+openButton3.addEventListener("click", () => {
+    dialogBox.showModal();
+    dialogBoxText.innerHTML = `One Banana contains 105 calories`
+});
+
+
+
 
 // close modal
 
