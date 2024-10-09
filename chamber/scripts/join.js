3ac6d9622e105c3f605ebbd4d46cf72f97142646
sil <narradoresdigitales@gmail.com>
2024-10-09 13:33:35 -0500

    updates to to join.html, css, and js

---
+++ chamber/scripts/join.js
@@ -0,0 +1,18 @@
+// JavaScript for modal functionality
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Close modals when clicking outside of them
+window.onclick = function(event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+}
