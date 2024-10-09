85c8342f8a7f7c734d5657c58f8ab06cee680152
sil <narradoresdigitales@gmail.com>
2024-10-09 14:59:51 -0500

    update thankyou.html / css

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,2 +1,11 @@
 const currentUrl = window.location.href;
 
+
+//Divide the url into two halves
+const everything=currentUrl.split('?')
+console.log(everything)
+console.log('hello')
+
+//Grab just the second half
+let formData = everything[1].split('&')
+console.log(formData)
