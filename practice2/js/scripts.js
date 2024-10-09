9a1352a831ef04d298d354de578c9d616bc7582e
sil <narradoresdigitales@gmail.com>
2024-10-09 09:49:30 -0500

    completed form data web lab

---
+++ practice2/js/scripts.js
@@ -0,0 +1,32 @@
+const currentUrl = window.location.href;
+
+
+//Divide the url into two halves
+const everything=currentUrl.split('?')
+
+
+//Grab just the second half
+let formData = everything[1].split('&')
+
+
+function show(cup) {
+    console.log(cup)
+    formData.forEach((element) => {
+       
+        if (element.startsWith(cup)) {
+            result=element.split('=')[1].replace("%40", "@",)
+            .replace(/\+/g, " ");
+        } //end if
+    })
+    return(result)
+} //end show
+
+
+const showInfo = document.querySelector('#results')
+showInfo.innerHTML = `
+<p>Appointment for: ${show("first")} ${show("last")}</p>
+<p>Proxy ${show("ordinance")} on ${show("fecha")} in the ${show("location")} Temple </p>
+<p>Your Phone: ${show("phone")}</p>
+<p>Your Email: <a href=mailto:${show("email")}">${show('email')}</p>
+`;
+
