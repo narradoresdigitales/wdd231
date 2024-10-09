3f65c76829164b143a56cc404e3e8c634eb391c5
sil <narradoresdigitales@gmail.com>
2024-10-09 17:15:05 -0500

    updated js for thankyou.js

--- practice2/js/scripts.js
+++ practice2/js/scripts.js
@@ -12,7 +12,7 @@ let formData = everything[1].split('&')
 function show(cup) {
     console.log(cup)
     formData.forEach((element) => {
-       
+    
         if (element.startsWith(cup)) {
             result=element.split('=')[1].replace("%40", "@",)
             .replace(/\+/g, " ");
