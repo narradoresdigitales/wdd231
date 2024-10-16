d99c32c1d8270a9ba91be98e2dddafc4337c296c
sil <narradoresdigitales@gmail.com>
2024-10-16 11:37:56 -0500

    edited navToggle.js function

--- websiteProject/scripts/navToggle.js
+++ websiteProject/scripts/navToggle.js
@@ -1,8 +1,8 @@
 // scripts/navToggle.js
 
 export function toggleNavMenu(buttonId, navId) {
-    const menuButton = document.getElementById(myButton);
-    const navList = document.getElementById(nav-links);
+    const menuButton = document.getElementById('myButton');
+    const navList = document.getElementById('nav-links');
 
     menuButton.addEventListener("click", () => {
         navList.classList.toggle("show"); // Toggle the 'show' class
